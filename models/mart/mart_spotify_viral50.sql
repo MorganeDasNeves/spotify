3bbcef9c61b0a7a5bf20b91ef32c5b9d31f9092e
Deborah Pierre <p.deborah95@gmail.com>
2026-03-12 11:54:40 +0000

    Création viral 10 + viral 50

---
+++ models/mart/mart_spotify_viral50.sql
@@ -0,0 +1,34 @@
+{{ config(materialized='table') }}
+SELECT
+    date_date
+    ,track_id
+    ,artist
+    ,title
+    ,album
+    ,release_date
+    ,ranking
+    ,CASE 
+    WHEN ranking <= 10 THEN 1 
+    ELSE 0 
+    END AS viral10
+    ,country
+    ,region
+    ,chart
+    ,trend
+    ,popularity
+    ,duration_ms
+    ,is_explicit
+    ,af_danceability
+    ,af_energy
+    ,af_speechiness
+    ,af_acousticness
+    ,af_instrumentalness
+    ,af_valence
+    ,af_tempo
+    ,cat_genre
+    ,potentiel_viral
+    ,energy_vibe
+    ,duration_segment
+    FROM {{ ref('mart_spotify__data_by_day') }}
+    WHERE chart= 'viral50' AND region = 'Europe'
+    ORDER BY date_date, ranking
